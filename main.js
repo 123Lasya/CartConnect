4fe0e915ad055b9c9c15fffd138c012cd0f9eaec
Mandadapu Lasya <m482719@gmail.com>
2026-01-05 22:06:23 +0530

    Updated all pages with the prvacy section

---
+++ main.js
@@ -0,0 +1,25 @@
+//FAQ Logic
+      document.addEventListener("DOMContentLoaded", () => {
+        const f_Items = document.querySelectorAll(".f-item");
+
+        f_Items.forEach((item) => {
+          const header = item.querySelector(".f-header");
+
+          header.addEventListener("click", () => {
+            // closeing all other items
+            f_Items.forEach((otherItem) => {
+              if (otherItem !== item) {
+                otherItem.classList.remove("active");
+              }
+            });
+
+            // toggle current item
+            item.classList.toggle("active");
+          });
+        });
+      });
+      // Dynamic Year for Footer
+const yearSpan = document.getElementById("current-year");
+if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+}
